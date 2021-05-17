125f1179d1cbf2cd7be447a319f4f69f87395b4e
VishalSingh1597 <80283079+VishalSingh1597@users.noreply.github.com>
2021-05-18 01:16:32 +0530

    [Vishal]Added:Calculating AVG Fare

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using CabInvoiceProgram;
+using CabInvoiceProgram;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
@@ -11,20 +11,20 @@ namespace Test
     public class Tests
     {
         /// <summary>
-        /// Givens the multiple rides should return invoice summary.  UC2
+        /// Givens the multiple rides should return invoice summary with average fare.  UC3
         /// </summary>
         [TestMethod]
-        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        public void GivenMultipleRidesShouldReturnInvoiceSummarywithAverageFare()
         {
             //Creating instance of invoice generator 
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
 
             //Generating Summary for rides
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
-            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0,15);
 
-            //Asserting values
+            //Asserting values with average in equals to formula in invoice summary
             Assert.AreEqual(expectedSummary, summary);
         }
     }
