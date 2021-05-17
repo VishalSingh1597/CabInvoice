e0f21194fca9987da9f7bccacaea86734c79ca49
Vishal Singh <VishalSingh1597>
2021-05-18 00:50:08 +0530

    [Vishal]Added:Welcome Code

---
+++ CabInvoiceProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceProgram
+{
+    /// <summary>
+    /// Design Principle 
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********Welcome to Cab Invoice Program***********");
+            //Creating Object
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            //Calculate Fare double
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare: {fare}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85574d19-e6c9-499e-a21b-e13f00e1ca13")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
